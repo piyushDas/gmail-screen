fa87e9ea2794aeb34d360a0030aab565f18d7db5
Piyush Das <piyush.das@cleartrip.com>
2020-04-27 12:23:50 +0530

    removing unnecessary component

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Login from './Containers/Login'
 import Mailbox from './Containers/Mailbox'
-import Readmail from './Containers/Readmail'
 
 const router = props => (
   <React.Fragment>
@@ -10,8 +9,6 @@ const router = props => (
       <Route exact={true} path="/" component={Login} />
       <Route exact={true} path="/login" component={Login} />
       <Route exact={true} path="/mailbox" component={Mailbox} />
-      <Route exact={true} path="/mailbox/read" component={Readmail} />
-      {/* <Route path="*" component={PageNotFound} /> */}
     </Switch>
   </React.Fragment>
 )
